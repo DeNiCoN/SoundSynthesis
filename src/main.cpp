d56f0291159bc356c42904a12f88c3c0a0cc2da2
DeNiCoN <denicon1234@gmail.com>
2020-10-03 20:36:04 +0300

    wav header

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+string outputFileName;
+
+int main(int argc, char *argv[]) {
+   
+    return 0;
+}
---
+++ src/wav_header.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <cstdint>
+
+namespace SSyntesis
+{
+
+    struct WAVHeader
+    {
+        uint8_t chunkId[4];
+        uint32_t chunkSize;
+
+        uint8_t format[4];
+        uint8_t subchunk1Id[4];
+        uint32_t subchunk1Size;
+        uint16_t audioFormat;
+        uint16_t numChannels;
+        uint32_t sampleRate;
+        uint32_t byteRate;
+        uint16_t blockAlign;
+        uint16_t bitsPerSample;
+
+        uint8_t subchunk2Id[4];
+        uint32_t subchunk2Size;
+    };
+
+    class PCMWAVHeader
+    {
+    public:
+        constexpr PCMWAVHeader(uint32_t numSamples,
+                               uint32_t sampleRate,
+                               uint16_t bitsPerSample,
+                               uint16_t numChannels = 1)
+            : m_header({
+                    .chunkId {'R', 'I', 'F', 'F'},
+                    // 36 is header size - 8
+                    .chunkSize = 36 + numSamples * numChannels * bitsPerSample / 8,
+                    .format {'W', 'A', 'V', 'E'},
+                    .subchunk1Id {'f', 'm', 't', ' '},
+                    .subchunk1Size = 16,
+                    .audioFormat = 1,
+                    .numChannels = numChannels,
+                    .sampleRate = sampleRate,
+                    .byteRate = numChannels * sampleRate * bitsPerSample / 8, //8 is byte size
+                    .blockAlign = static_cast<unsigned short>(numChannels * bitsPerSample / 8),
+                    .bitsPerSample = bitsPerSample,
+                    .subchunk2Id {'d', 'a', 't', 'a'},
+                    .subchunk2Size = numSamples * numChannels * bitsPerSample / 8
+                })
+        {}
+    private:
+        WAVHeader m_header;
+    };
+
+}
