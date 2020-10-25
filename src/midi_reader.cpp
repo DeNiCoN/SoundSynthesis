4c6992d3c4c23a9cecdea0b72eeaa2b6da908980
DeNiCoN <denicon1234@gmail.com>
2020-10-25 14:18:47 +0200

    Move reading to midi_reader files

--- src/midi_reader.cpp
+++ src/midi_reader.cpp
@@ -19,5 +19,42 @@ namespace SSynthesis
             m_header = HeaderData::readFromStream(in);
            
         }
+
+        ChunkHeader ChunkHeader::readFromStream(istream &in)
+        {
+            ChunkHeader result;
+            in.read(result.m_chunkID, sizeof(result.m_chunkID));
+            result.m_chunkSize = readBigEndian<typeof(result.m_chunkSize)>(in);
+            return result;
+        }
+
+        HeaderData HeaderData::readFromStream(istream &in)
+        {
+            HeaderData result;
+            result.m_formatType =
+                static_cast<FormatType>(readBigEndian<uint16_t>(in));
+            result.m_numberOfTracks =
+                readBigEndian<typeof(result.m_numberOfTracks)>(in);
+
+            auto time = readBigEndian<uint16_t>(in);
+            result.m_isFramesPerSecond = time & 0x8000;
+            result.m_rawTimeInfo = time & 0x7FFF;
+            return result;
+        }
+
+        uint32_t readVariableLength(std::istream& in)
+        {
+            uint32_t result = 0;
+            char next;
+            in.read(&next, 1);
+            result |= next & 0x7F;
+            while (next & 0x80)
+            {
+                result = result << 7;
+                in.read(&next, 1);
+                result |= next & 0x7F;
+            }
+            return result;
+        };
     }
 }
--- src/midi_reader.hpp
+++ src/midi_reader.hpp
@@ -1,20 +1,111 @@
 #pragma once
-#include "midi.hpp"
 #include <iostream>
+#include <cassert>
 
 namespace SSynthesis
 {
     namespace MIDI
     {
+        class ChunkHeader
+        {
+        public:
+            static ChunkHeader readFromStream(std::istream& in);
+
+            std::string getChunkID() const
+            { return std::string(m_chunkID, sizeof(m_chunkID)); };
+
+            uint32_t getChunkSize() const { return m_chunkSize; }
+
+            static constexpr const char* headerID = "MThd";
+            static constexpr const char* trackID = "MTrk";
+
+
+        private:
+            char m_chunkID[4];
+            uint32_t m_chunkSize;
+
+            //Only reading is implemented
+            ChunkHeader() = default;
+        };
+
+        enum class FormatType : uint16_t
+        {
+            ONE_TRACK = 0,
+            PARALLEL_TRACKS = 1,
+            SEQUENTIAL_TRAKS = 2
+        };
+
+        class HeaderData
+        {
+        public:
+            static HeaderData readFromStream(std::istream& in);
+            bool isFramesPerSecond() const { return m_isFramesPerSecond; }
+            uint16_t getTicksPerByte() const { return m_rawTimeInfo; }
+            uint16_t getFrames() const { return m_rawTimeInfo & 0x7F00; }
+            uint16_t getTicks() const { return m_rawTimeInfo & 0x00FF; }
+            FormatType getFormatType() const { return m_formatType; }
+            uint16_t getNumOfTracks() const { return m_numberOfTracks; }
+
+        private:
+            // "MThd" and 6
+            FormatType m_formatType; // 0-2
+            uint16_t m_numberOfTracks; // 1-65,535
+
+            bool m_isFramesPerSecond;
+            uint16_t m_rawTimeInfo;
+        };
+
+        enum class EventType
+        {
+            NOTE_OFF = 0x8,
+            NOTE_ON = 0x9,
+            NOTE_AFTERTOUCH = 0xA,
+            CONTROLLER = 0xB,
+            PROGRAM_CHANGE = 0xC,
+            CHANNEL_AFTERTOUCH = 0xD,
+            PITCH_BEND = 0xE,
+        };
+
+        struct EventHead
+        {
+            uint8_t eventType : 4;
+            uint8_t MIDIChannel : 4;
+        };
+
+        struct ChannelEvent
+        {
+            EventType eventType;
+            uint8_t MIDIChannel;
+            //Starts vith variable-length delta time
+            unsigned int firstPar;
+            unsigned int secondPar;
+        };
+
+        template <typename Integer>
+        Integer readBigEndian(std::istream& in, size_t bytes = sizeof(Integer))
+        {
+            assert(bytes > 0 && bytes <= sizeof(Integer));
+            uint8_t raw[sizeof(Integer)];
+            in.read(reinterpret_cast<char*>(raw), bytes);
+            Integer result = raw[0];
+            for (size_t i = 1; i < bytes; i++)
+            {
+                result = (result << 8) | raw[i];
+            }
+            return result;
+        }
+
         class Reader
         {
         public:
             //Reads header from input
             Reader(std::istream& input);
-           
+
         private:
             HeaderData m_header;
         };
-       
+
+        uint32_t readVariableLength(std::istream& in);
+
     }
 }
